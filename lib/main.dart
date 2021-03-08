1f4d331189254a45c94ab54e13bf5fe9eb05cbf0
pkk1113 <pkk1113@naver.com>
2021-03-08 21:41:50 +0900

    [update] life cycle 간단하게 학습

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
+    print("1 - StatefulWidget::createState()");
     return _MyApp();
   }
 }
@@ -17,7 +18,21 @@ class _MyApp extends State<MyApp> {
   Color color = Colors.red[400];
 
   @override
+  void initState() {
+    super.initState();
+
+    print("2 -  State::initState()");
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print("3 -  State::didChangeDependencies()");
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print("4 -  State::build()");
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(primarySwatch: Colors.blue),
@@ -41,5 +56,23 @@ class _MyApp extends State<MyApp> {
                     },
                     style: ElevatedButton.styleFrom(
                         primary: this.color, minimumSize: Size(300, 300))))));
+  }
+
+  @override
+  void didUpdateWidget(covariant MyApp oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print("5 -  didUpdateWidget");
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+    print("deactivate()");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print("dispose()");
   }
 }
