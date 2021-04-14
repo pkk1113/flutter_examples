0585ed1d373e308da8caa8c545ed420bb0a42875
pkk1113 <pkk1113@naver.com>
2021-04-14 23:35:44 +0900

    [add] stateful 예시 추가

--- lib/main.dart
+++ lib/main.dart
@@ -8,108 +8,179 @@ class Item {
   Item({this.str});
 }
 
-class Controller extends GetxController {
-  static Controller get to => Get.find();
-  RxList<Item> items = RxList<Item>();
+class ItemWidget extends StatelessWidget {
+  final Key key;
+  final int index;
+  final String str;
+  final bool isLast;
+
+  ItemWidget({this.index, this.str, this.isLast}) : key = Key('$index');
 
   @override
-  void onInit() {
+  Widget build(BuildContext context) {
+    print(str);
+    return Material(
+      child: Container(
+        key: key,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5.0),
+        height: 50,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              // child: Text(str),
+              child: TextFormField(
+                  key: Key(str),
+                  initialValue: str,
+                  textInputAction: TextInputAction.next,
+                  maxLines: 1,
+                  onFieldSubmitted: (value) {},
+                  onEditingComplete: () {
+                    return isLast ? Get.focusScope.unfocus() : Get.focusScope.nextFocus();
+                  }),
+            ),
+            SizedBox(width: 10),
+            GestureDetector(
+              onTapDown: (_) => Get.focusScope.unfocus(),
+              child: ReorderableDragStartListener(
+                child: const Icon(
+                  Icons.drag_handle,
+                  size: 30,
+                ),
+                index: index,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  var items = [];
+  TextEditingController textEditorController;
+
+  @override
+  void initState() {
     items
       ..add(Item(str: '감자'))
       ..add(Item(str: '냉이'))
       ..add(Item(str: '된장'))
       ..add(Item(str: '라면'))
       ..add(Item(str: '미역'));
-
-    debounce(items, (_) => print('!!'), time: Duration(seconds: 2));
-    super.onInit();
+    textEditorController = TextEditingController();
+    super.initState();
   }
-}
 
-class App extends StatelessWidget {
+  @override
+  void dispose() {
+    textEditorController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final ctrl = Get.put(Controller());
-    String str;
-    print('App!!');
-
+    print('app build');
     return Scaffold(
       appBar: AppBar(title: Text('테스트!')),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          Get.dialog(
-              AlertDialog(
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6.0)),
-                contentPadding: EdgeInsets.only(left: 15, right: 15, top: 10, bottom: 15),
-                content: SizedBox(
-                  width: 0,
-                  child: ListView(
-                    physics: NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    children: [
-                      TextField(
-                        onChanged: (value) {
-                          print(value);
-                          return str = value;
-                        },
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Expanded(
-                              child: OutlinedButton(
-                                  onPressed: () {
-                                    Get.back();
-                                  },
-                                  child: Text('취소'))),
-                          SizedBox(width: 5),
-                          Expanded(
-                              child: OutlinedButton(
-                                  onPressed: () {
-                                    ctrl.items.add(Item(str: str));
-                                    Get.back();
-                                  },
-                                  child: Text('확인'))),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              barrierDismissible: true);
-        },
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  items[0] = Item(str: '고구마');
+                });
+              },
+              child: Icon(Icons.change_history)),
+          SizedBox(width: 10),
+          FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  items.removeLast();
+                });
+              },
+              child: Icon(Icons.remove)),
+          SizedBox(width: 10),
+          FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () => _showDialog(),
+          ),
+        ],
       ),
-      body: Obx(
-        () => ReorderableListView.builder(
+      body: GestureDetector(
+        onTap: () {
+          Get.focusScope.unfocus();
+        },
+        child: ReorderableListView.builder(
           itemBuilder: (context, index) {
-            return Container(
-              padding: EdgeInsets.all(10.0),
-              key: Key('$index'),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(ctrl.items[index].str),
-                  ReorderableDragStartListener(
-                      child: const Icon(
-                        Icons.drag_handle,
-                        size: 30,
-                      ),
-                      index: index)
-                ],
-              ),
+            return ItemWidget(
+              index: index,
+              str: items[index].str,
+              isLast: items.length - 1 == index,
             );
           },
-          itemCount: ctrl.items.length,
+          itemCount: items.length,
           onReorder: (oldIndex, newIndex) {
-            if (oldIndex < newIndex) {
-              newIndex -= 1;
-            }
-            final item = ctrl.items.removeAt(oldIndex);
-            ctrl.items.insert(newIndex, item);
+            setState(() {
+              if (oldIndex < newIndex) {
+                newIndex -= 1;
+              }
+              final item = items.removeAt(oldIndex);
+              items.insert(newIndex, item);
+            });
           },
         ),
       ),
+    );
+  }
+
+  _showDialog() {
+    Get.dialog(
+      AlertDialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6.0)),
+        contentPadding: EdgeInsets.only(left: 15, right: 15, top: 10, bottom: 15),
+        content: SizedBox(
+          width: 0,
+          child: ListView(
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            children: [
+              TextField(
+                controller: textEditorController,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                      child: OutlinedButton(
+                          onPressed: () {
+                            Get.back();
+                          },
+                          child: Text('취소'))),
+                  SizedBox(width: 5),
+                  Expanded(
+                    child: OutlinedButton(
+                      onPressed: () {
+                        textEditorController.clear();
+                        Get.back();
+                      },
+                      child: Text('확인'),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+      barrierDismissible: true,
     );
   }
 }
